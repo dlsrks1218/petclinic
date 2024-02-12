d4db915fafe77a4d6482c2e50c6eebf791616e3e
dlsrks1218 <dlsrks1218@daum.net>
2024-02-12 21:06:40 +0900

    스크립트 및 삭제 정책 수정

--- k8s-cleanup.sh
+++ k8s-cleanup.sh
@@ -1,3 +1,41 @@
 #!/bin/sh
+# kubectl delete pv mysql-storage-pv mysql-config-pv
+# kubectl delete pvc mysql-storage-mysql-0 mysql-config-mysql-0
 kubectl delete -f k8s/db -f k8s/app -f k8s/ingress --recursive
 
+# Function to check if a command exists
+command_exists() {
+    command -v "$@" >/dev/null 2>&1
+}
+
+# Check if Minikube is installed
+if command_exists minikube; then
+    # Find any running Minikube tunnel processes
+    TUNNEL_PID=$(pgrep -f "minikube tunnel")
+    if [ ! -z "$TUNNEL_PID" ]; then
+        kill $TUNNEL_PID
+        echo "Minikube tunnel process killed."
+    else
+        echo "No Minikube tunnel process found."
+    fi
+
+    # Find any running kubectl proxy processes
+    PROXY_PID=$(pgrep -f "kubectl proxy")
+    if [ ! -z "$PROXY_PID" ]; then
+        kill $PROXY_PID
+        echo "Kubectl proxy process killed."
+    else
+        echo "No kubectl proxy process found."
+    fi
+else
+    echo "Minikube is not installed."
+fi
+
+# Check and delete if "www.test.com" exists in /etc/hosts
+if grep -q "www.test.com" /etc/hosts; then
+    echo "Removing line containing 'www.test.com' from /etc/hosts..."
+    sudo sed -i "/www.test.com/d" /etc/hosts
+    echo "Line containing 'www.test.com' removed from /etc/hosts."
+else
+    echo "'www.test.com' not found in /etc/hosts."
+fi
--- k8s-deploy.sh
+++ k8s-deploy.sh
@@ -1,19 +1,44 @@
 #!/bin/sh
 kubectl apply -f k8s/db -f k8s/app --recursive
 
+# Function to check if a command exists
+command_exists() {
+    command -v "$@" > /dev/null 2>&1
+}
+
+# Check if Minikube is installed
+if command_exists minikube; then
+    # Use Minikube's IP
+    CURRENT_IP=$(minikube ip)
+    # Set proxy, tunneling for Minikube
+    echo "Set proxy, tunnel for minikube"
+    kubectl proxy --address 0.0.0.0 --port 30001 --accept-hosts='^*$' >/dev/null 2>&1 &
+    minikube tunnel >/dev/null 2>&1 &
+else
+    # Use Bare Metal Kubernetes Node's IP
+    CURRENT_IP=$(hostname -I | awk '{print $2}')
+fi
+
 # get current IP
-CURRENT_IP=$(hostname -I | awk '{print $1}')
+CURRENT_IP=$(hostname -I | awk '{print $2}')
 
 # Append the entry to /etc/hosts
 cat <<EOF >> /etc/hosts
 $CURRENT_IP www.test.com
 EOF
 
-echo "Added $CURRENT_IP www.test.com to /etc/hosts"
+echo "Add $CURRENT_IP www.test.com to /etc/hosts"
 
 kubectl apply -f k8s/ingress --recursive
 
+echo "Wait for ingress-nginx resource.."
 kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=120s
+
+echo "Wait for mysql pod to be ready..."
+kubectl wait --for=condition=ready pod --timeout=120s -l app=mysql
+
+echo "Wait for petclinic pod to be ready..."
+kubectl wait --for=condition=ready pod --timeout=120s -l app=petclinic
