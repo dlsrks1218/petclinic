8c5bdace4902da4c3544c40c7de31ab4405a4ad9
dlsrks1218 <dlsrks1218@daum.net>
2024-02-13 12:03:27 +0900

    스크립트 수정

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+# Set up DB for local build test and Shut down when the test ends.
+# Gradle App & Docker Image Build. Push the image to docker hub registry.
 ./gradlew DBInit clean build jib DBShutDown
-kubectl apply -f db/. -f app/.
+kubectl apply -f k8s/db/.
+kubectl apply -f k8s/app/.
